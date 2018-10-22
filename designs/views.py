cda23b011c36bd63821bc65d5b2e83c585f2b18b
Sam Oluoch <samsoluoch@gmail.com>
2018-10-22 15:53:12 +0300

    created view sunction for the user sign up

--- designs/views.py
+++ designs/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 
 # Create your views here.
 def home(request):
     # posts = Post.objects.all()
 
     return render(request,'home.html')
+
+def register(request):
+    if request.user.is_authenticated():
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            form = RegistrationForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                # user.is_active = False
+                user.save()
+                # current_site = get_current_site(request)
+                # to_email = form.cleaned_data.get('email')
+                # activation_email(user, current_site, to_email)
+                # return HttpResponse('Please confirm your email')
+            return redirect('home.html')
+
+        else:
+            form = RegistrationForm()
+        return render(request, 'registration/signup.html',{'form':form})
